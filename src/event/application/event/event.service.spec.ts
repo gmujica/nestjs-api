9729669505ae49a117c180e18dbb1345a52b97d7
gmujica <gregorymujica1993@gmail.com>
2023-07-27 17:04:30 -0300

    event test fixed

--- src/event/application/event/event.service.spec.ts
+++ src/event/application/event/event.service.spec.ts
@@ -1,18 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventService } from './event.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Event } from '../../infrastructure/entity/event.entity';
 
 describe('EventService', () => {
   let service: EventService;
+  let eventRepository: Repository<Event>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventService],
+      providers: [
+        EventService,
+        {
+          provide: getRepositoryToken(Event),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<EventService>(EventService);
+    eventRepository = module.get<Repository<Event>>(getRepositoryToken(Event));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+    it('should return an array of events', async () => {
+      const events: Event[] = [
+        { event_id: '1', title: 'Event 1', descrption: 'Description 1', id: '1', created_at: new Date(), updated_at: new Date() },
+        { event_id: '2', title: 'Event 2', descrption: 'Description 2', id: '2', created_at: new Date(), updated_at: new Date() },
+      ];
+      jest.spyOn(eventRepository, 'find').mockResolvedValue(events);
+
+      const result = await service.findAll();
+      expect(result).toEqual(events);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single event when a valid event_id is provided', async () => {
+      const eventId = '1';
+      const event: Event = { event_id: eventId, title: 'Event 1', descrption: 'Description 1', id: '1', created_at: new Date(), updated_at: new Date() };
+      jest.spyOn(eventRepository, 'findOne').mockResolvedValue(event);
+
+      const result = await service.findOne(eventId);
+      expect(result).toEqual(event);
+    });
+
+    it('should return null when event is not found', async () => {
+      const eventId = 'invalid-event-id';
+      jest.spyOn(eventRepository, 'findOne').mockResolvedValue(null);
+
+      const result = await service.findOne(eventId);
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new event and return it', async () => {
+      const newEvent: Event = { event_id: '1', title: 'New Event', descrption: 'New Description', id: '3', created_at: new Date(), updated_at: new Date() };
+      const createdEvent: Event = { event_id: '3', title: 'New Event', descrption: 'New Description', id: '3', created_at: new Date(), updated_at: new Date() };
+
+      jest.spyOn(eventRepository, 'create').mockReturnValue(newEvent);
+      jest.spyOn(eventRepository, 'save').mockResolvedValue(createdEvent);
+
+      const result = await service.create(newEvent);
+      expect(result).toEqual(createdEvent);
+    });
+  });
 });
+
--- src/event/controllers/event/event.controller.spec.ts
+++ src/event/controllers/event/event.controller.spec.ts
@@ -2,28 +2,38 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EventController } from './event.controller';
 import { EventService } from '../../application/event/event.service';
 import { Event } from '../../infrastructure/entity/event.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('EventController', () => {
   let controller: EventController;
-  let service: EventService;
+  let eventService: EventService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EventController],
-      providers: [EventService],
+      providers: [ EventService,
+        {
+          provide: getRepositoryToken(Event), // Provide the EventRepository token
+          useClass: jest.fn(),
+        },
+      ],
     }).compile();
 
     controller = module.get<EventController>(EventController);
-    service = module.get<EventService>(EventService);
+    eventService = module.get<EventService>(EventService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
   });
 
   describe('findAll', () => {
     it('should return an array of events', async () => {
       const events: Event[] = [
-        { event_id: '1', title: 'Event 1', descrption: 'Description 1', id: 'user1', created_at: new Date(), updated_at: new Date() },
-        { event_id: '2', title: 'Event 2', descrption: 'Description 2', id: 'user2', created_at: new Date(), updated_at: new Date() },
+        { event_id: '1', title: 'Event 1', descrption: 'Description 1', id: '1', created_at: new Date(), updated_at: new Date() },
+        { event_id: '2', title: 'Event 2', descrption: 'Description 2', id: '2', created_at: new Date(), updated_at: new Date() },
       ];
-      jest.spyOn(service, 'findAll').mockResolvedValue(events);
+      jest.spyOn(eventService, 'findAll').mockResolvedValue(events);
 
       const result = await controller.findAll();
       expect(result).toEqual(events);
@@ -33,15 +43,8 @@ describe('EventController', () => {
   describe('findOne', () => {
     it('should return a single event when a valid event_id is provided', async () => {
       const eventId = '1';
-      const event: Event = {
-        event_id: eventId,
-        title: 'Event 1',
-        descrption: 'Description 1',
-        id: 'user1',
-        created_at: new Date(),
-        updated_at: new Date(),
-      };
-      jest.spyOn(service, 'findOne').mockResolvedValue(event);
+      const event: Event = { event_id: eventId, title: 'Event 1', descrption: 'Description 1', id: '1', created_at: new Date(), updated_at: new Date() };
+      jest.spyOn(eventService, 'findOne').mockResolvedValue(event);
 
       const result = await controller.findOne(eventId);
       expect(result).toEqual(event);
@@ -49,7 +52,7 @@ describe('EventController', () => {
 
     it('should throw an error when event is not found', async () => {
       const eventId = 'invalid-event-id';
-      jest.spyOn(service, 'findOne').mockResolvedValue(null);
+      jest.spyOn(eventService, 'findOne').mockResolvedValue(null);
 
       await expect(controller.findOne(eventId)).rejects.toThrow('Event not found');
     });
@@ -57,25 +60,10 @@ describe('EventController', () => {
 
   describe('create', () => {
     it('should create a new event and return it', async () => {
-      const newEvent: Event = {
-        title: 'New Event',
-        descrption: 'New Description',
-        event_id: 'user3',
-        id: 'id1',
-        created_at: new Date(),
-        updated_at: new Date(),
-      };
+      const newEvent: Event = { event_id: '1', title: 'New Event', descrption: 'New Description', id: '3', created_at: new Date(), updated_at: new Date() };
+      const createdEvent: Event = { event_id: '3', title: 'New Event', descrption: 'New Description', id: '3', created_at: new Date(), updated_at: new Date() };
 
-      const createdEvent: Event = {
-        event_id: '3',
-        title: 'New Event',
-        descrption: 'New Description',
-        id: 'user3',
-        created_at: new Date(),
-        updated_at: new Date(),
-      };
-
-      jest.spyOn(service, 'create').mockResolvedValue(createdEvent);
+      jest.spyOn(eventService, 'create').mockResolvedValue(createdEvent);
 
       const result = await controller.create(newEvent);
       expect(result).toEqual(createdEvent);
