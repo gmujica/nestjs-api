b2602b789b724e4ebec8403d2f8451931ae48228
gmujica <gregorymujica1993@gmail.com>
2023-08-12 16:03:58 -0300

    update and delete event

--- src/event/application/event/event.service.ts
+++ src/event/application/event/event.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Event } from '../../infrastructure/entity/event.entity'
 import { InjectRepository } from '@nestjs/typeorm';
@@ -27,4 +27,22 @@ export class EventService {
         const savedEvent = await this.eventRepository.save(newEvent);
         return await this.eventRepository.save(savedEvent);
     }
+    //update event
+    async updateEvent(event_id: string, updatedEvent: Event): Promise<Event> {
+        const existingEvent = await this.findOne(event_id);
+        if (!existingEvent) {
+            throw new NotFoundException(`Event with ID ${event_id} not found.`);
+        }
+        const mergedEvent = this.eventRepository.merge(existingEvent, updatedEvent);
+        return await this.eventRepository.save(mergedEvent);
+    }
+    //delete event
+    async deleteEvent(event_id: string): Promise<void> {
+        const existingEvent = await this.findOne(event_id);
+        if (!existingEvent) {
+            throw new NotFoundException(`Event with ID ${event_id} not found.`);
+        }
+        await this.eventRepository.remove(existingEvent);
+    }
+
 }
